5d8bb3b55428640acf5d78bfb67ef4b5aca4c382
DwEnn <prgohn@gmail.com>
2018-12-15 17:41:31 +0900

    upload B11654

---
+++ src/BaekJoon/B11654.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B11654 {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		System.out.println((int) sc.nextLine().charAt(0));
+		sc.close();
+	}
+
+}
